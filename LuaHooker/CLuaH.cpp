b8e768d6432f74a87cf35a3c320fd86cc8898157
Fabio3rs <fabio3rs@gmail.com>
2015-03-25 00:39:08 -0300

    start test

--- LuaHooker/CLuaH.cpp
+++ LuaHooker/CLuaH.cpp
@@ -18,6 +18,23 @@ bool CLuaH::loadFiles(const std::string &path)
 		return std::string((s = fname.find_last_of('.') != fname.npos)? &fname.c_str()[++s] : "");
 	};
 
+	HANDLE hFind;
+	WIN32_FIND_DATA data;
+
+	hFind = FindFirstFile("c:\\*.*", &data);
+	if (hFind != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if ((data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0
+				&& extension_from_filename(data.cFileName) == "lua")
+			{
+
+			}
+		} while (FindNextFile(hFind, &data));
+		FindClose(hFind);
+	}
+
 }
 
 CLuaH::CLuaH()
