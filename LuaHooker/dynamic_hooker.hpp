4138d29cfb64e1297da8db2954e8e8eed9850b66
Fabio3rs <fabio3rs@gmail.com>
2016-02-12 22:45:09 -0200

    dynamic_hooker.hpp

---
+++ LuaHooker/dynamic_hooker.hpp
@@ -0,0 +1,180 @@
+#pragma once
+#ifndef _LUA_HOOKER_DYNAMIC_HOOKER_H_
+#define _LUA_HOOKER_DYNAMIC_HOOKER_H_
+#include <cstdint>
+#include <injector\assembly.hpp>
+#include <injector\calling.hpp>
+#include <injector\injector.hpp>
+#include <algorithm>
+#include <string>
+
+namespace injectcode{
+	typedef void(__cdecl *callback_t)(injector::reg_pack&, uintptr_t address);
+
+	static uintptr_t camefromaddress;
+	static uintptr_t usrcall;
+	static uintptr_t retnptr;
+
+	inline void callwrapper(injector::reg_pack *r)
+	{
+		callback_t c = (callback_t)usrcall;
+		c(*r, camefromaddress);
+	}
+
+	// Constructs a reg_pack and calls the wrapper functor
+	static inline void __declspec(naked) make_reg_pack_and_call()
+	{
+		_asm
+		{
+			pop retnptr
+				pop usrcall
+				pop camefromaddress
+				// Construct the reg_pack structure on the stack
+				pushad              // Pushes general purposes registers to reg_pack
+				add[esp + 12], 4     // Add 4 to reg_pack::esp 'cuz of our return pointer, let it be as before this func is called
+				pushfd              // Pushes EFLAGS to reg_pack
+
+				// Call wrapper sending reg_pack as parameter
+				push esp
+				call callwrapper
+				add esp, 4
+
+				// Destructs the reg_pack from the stack
+				sub[esp + 12 + 4], 4   // Fix reg_pack::esp before popping it (doesn't make a difference though) (+4 because eflags)
+				popfd               // Warning: Do not use any instruction that changes EFLAGS after this (-> sub affects EF!! <-)
+				popad
+
+				// Back to normal flow
+				ret
+		}
+	}
+
+	static inline void __declspec(naked) make_reg_pack_and_call_with_return()
+	{
+		_asm
+		{
+			pop retnptr
+				pop usrcall
+				pop camefromaddress
+
+				// Construct the reg_pack structure on the stack
+				pushad              // Pushes general purposes registers to reg_pack
+				add[esp + 12], 4     // Add 4 to reg_pack::esp 'cuz of our return pointer, let it be as before this func is called
+				pushfd              // Pushes EFLAGS to reg_pack
+
+				// Call wrapper sending reg_pack as parameter
+				push esp
+				call callwrapper
+				add esp, 4
+
+				// Destructs the reg_pack from the stack
+				sub[esp + 12 + 4], 4   // Fix reg_pack::esp before popping it (doesn't make a difference though) (+4 because eflags)
+				popfd               // Warning: Do not use any instruction that changes EFLAGS after this (-> sub affects EF!! <-)
+				popad
+
+				// jump
+				push retnptr
+				retn
+		}
+	}
+
+	class dynamic_hooker{
+
+	#pragma pack(push, 1)
+		struct code
+		{
+			int8_t push;
+			uintptr_t pushaddr;
+			int8_t push2;
+			uintptr_t useraddr;
+			int8_t push3;
+			uintptr_t retaddr;
+			int8_t jmp;
+			uintptr_t jmpaddr;
+
+			inline code()
+			{
+				push = 0x68;
+				pushaddr = 0;
+
+				push2 = 0x68;
+				useraddr = 0;
+
+				push3 = 0x68;
+				retaddr = 0;
+
+				jmp = 0xE8;
+				pushaddr = 0;
+			}
+		};
+	#pragma pack(pop)
+
+		struct codstack
+		{
+			code cdsrc;
+			bool isused;
+
+			inline codstack()
+			{
+				isused = false;
+			}
+		};
+
+
+		code *codelist;
+		size_t codelistcapacity, codelistsize;
+
+
+	public:
+		inline void addhook(uintptr_t address, callback_t c)
+		{
+			if (codelistsize >= codelistcapacity)
+			{
+				auto capacity = codelistcapacity;
+				codelistcapacity *= 2;
+
+				code *ncodelst = new code[codelistcapacity];
+
+				std::move(codelist, codelist + capacity, ncodelst);
+
+				delete[] codelist;
+				codelist = ncodelst;
+			}
+
+			code &ncode = codelist[codelistsize++];
+
+			ncode.pushaddr = address;
+			ncode.useraddr = (uintptr_t)c;
+			ncode.retaddr = (uintptr_t)(void*)injector::MakeCALL((void*)address, (void*)&ncode).get();
+
+			if (ncode.retaddr != NULL)
+			{
+				injector::MakeJMP(&ncode.jmp, make_reg_pack_and_call_with_return, false);
+			}
+			else
+			{
+				injector::MakeJMP(&ncode.jmp, make_reg_pack_and_call, false);
+			}
+		}
+
+		inline dynamic_hooker()
+		{
+			codelistcapacity = 8;
+			codelistsize = 0;
+			codelist = new code[codelistcapacity];
+		}
+
+		inline ~dynamic_hooker()
+		{
+			codelistcapacity = 0;
+			codelistsize = 0;
+			delete[] codelist;
+		}
+	};
+
+
+}
+
+#endif
+
+
